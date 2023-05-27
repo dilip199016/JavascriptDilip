90354f180142238d72294fd66185684b3dedbf89
Dilip Varma <vddilipvarma@gmail.com>
2023-05-27 19:17:19 +0530

    tutorial task

---
+++ 2-lectureja.js
@@ -0,0 +1,13 @@
+console.log("Hey");
+
+ver detail=20+"DILIP";
+
+const lastName="Varma";
+const middName="";
+if(middName){
+    console.log("Hey having middle Name");
+
+}
+else if(lastName){
+    console.log("Hey Having Last Name");
+}
